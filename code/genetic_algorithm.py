ff327ac4d905670b85fe64a8ba9b6f4c59b0d612
Tritryi <tristan.fontaniere@outlook.fr>
2026-05-03 01:44:51 +0200

    Score is now computed instead of distance to exit + comments refining

--- code/genetic_algorithm.py
+++ code/genetic_algorithm.py
@@ -1,5 +1,6 @@
 from maze import Maze
 import numpy as np
+import matplotlib.pyplot as plt
 mazeObject = Maze()
 
 """
@@ -16,6 +17,7 @@ We use a genome of 10 genes because the objective is to prove that it can do bet
 population_size = 100
 genome_length = 10
 mutation_rate = 0.05
+best_score_per_gen = []
 
 # Step 1 : populate
 # creates 100 individuals : 100 tables which contains 30 values from 0 to 3
@@ -37,6 +39,7 @@ start_x = mazeObject.getX()
 start_y = mazeObject.getY() 
 
 for gen in range(10):
+    # to store distances
     all_distances = []  
 
     # for each individual
@@ -44,6 +47,7 @@ for gen in range(10):
         # initialize x and y
         x = start_x
         y = start_y
+        steps = 0 
         
         # for each gene (move)
         for move in p:
@@ -55,32 +59,32 @@ for gen in range(10):
             
             # checking that we are not our of the maze
             if 0 <= next_Y < len(maze) and 0 <= next_X < len(maze[0]):
-                # if the cell it targets is not a wall, we go on it. Otherwise, it does not move
+                # if the cell targeted is not a wall, it goes on it. Otherwise, it does not move
+                steps +=1
                 if maze[next_Y][next_X] == 0:
                     x = next_X
                     y = next_Y
             # if the end was reached, no need to keep going
             if x == 3 and y == 0:
                 break
-        # computes how close the individual is to the exit, we use absolute so that no difference is made between left/right or up/down
-        dist = abs(x-3) + abs(y-0)
-        all_distances.append(dist)
+        # computes a score based on distance and steps done, the first part computes the distance to the exit, the second one is used to include steps in the result
+        # with this if A : 0 + (9*0.1) and B : 0 + (6*0.1), B will get a better score because it requires less steps
+        score = (abs(x-3) + abs(y-0)) + (steps*0.1)
+        all_distances.append(score)
         
     # converts the distance array to a numpy array    
     distances_array = np.array(all_distances)
-    # sort the results, closest individuals first, gives an indexes array 
+    # sort the results, best scored individuals first, gives an indexes array 
     sorted_results = np.argsort(distances_array)
+    best_score_per_gen.append(distances_array[sorted_results[0]])
 
     print(f"--- Generation {gen} ---")
-    print(f"Best individual : #{sorted_results[0]}, with {distances_array[sorted_results[0]]} steps away from the exit")
+    print(f"Best individual : #{sorted_results[0]}, with {distances_array[sorted_results[0]]*10:.0f} steps away from the exit")
     print("-" *10)
 
-    # Step 3 : Next Generation, the crossover
-    best_individuals = sorted_results[:20]
-    
-    # preparing list for next generation 
+    # Step 3 : Next Generation, the crossover    
+    # preparing next generation and making sure best individual survives
     new_generation = []
-    # saves the best inddividual
     new_generation.append(population[sorted_results[0]])
     
     # creating the new generation individuals
@@ -93,17 +97,42 @@ for gen in range(10):
         parent1 = population[sorted_results[index_parent1]]
         parent2 = population[sorted_results[index_parent2]]
         
-        # crossover 
+        # crossover : take a random index
         point = np.random.randint(1, genome_length)
-        # creating a child
+        # creating a child, parent1 gives 0 to point genes and parent2 gives point to genome_length genes
         child = np.concatenate([parent1[:point], parent2[point:]])
         
         # mutation        
         for i in range(genome_length):
+            # takes a random number, if below mutation_rate a mutation occurs
             if np.random.rand() <= mutation_rate:
                 child[i] = np.random.randint(0,4)
         
         new_generation.append(child)
-        
+    # the next population is set as the new one we created
     population = np.array(new_generation)
-        
+
+
+plt.figure(figsize=(12,5))
+plt.subplot(1,2,1)
+plt.plot(best_score_per_gen, label="Genetic algorithm", color="blue", linewidth=2)
+plt.title("Learning")
+plt.xlabel("Generation")
+plt.ylabel("Score (Distance + Steps)")
+plt.legend()
+plt.grid(True, alpha=0.3)
+
+plt.subplot(1, 2, 2)
+final_ia_steps = best_score_per_gen[-1]*10
+methods = ["Right hand", "Genetic Algorithm"]
+steps = [28, final_ia_steps]
+
+plt.bar(methods, steps, color=["red","green"])
+plt.title("Final comparison")
+plt.ylabel("Number of steps to exit")
+
+for i,v in enumerate(steps):
+    plt.text(i, v + 0.5, str(int(v)), ha='center', fontweight='bold')
+
+plt.tight_layout()
+plt.show()
