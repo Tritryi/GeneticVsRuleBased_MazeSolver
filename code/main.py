4403fd4cdad6e29acb9f137c5d6a8977727c8e3c
Tritryi <tristan.fontaniere@outlook.fr>
2026-05-04 20:34:10 +0200

    output optimization

--- code/main.py
+++ code/main.py
@@ -3,9 +3,44 @@ from right_hand_rule import executeRigthtHand
 from maze import Maze
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 mazeObject = Maze()
 
+def printGaMaze(genome):
+    mazeObject.clear()
+    mazeObject.printMaze()
+    time.sleep(0.3)
+    directions = [
+        (0, -1), # 0 = North
+        (1, 0),  # 1 = East
+        (0, 1),  # 2 = South
+        (-1, 0), # 3 = West
+    ]
+    x= mazeObject.getX()
+    y= mazeObject.getY()
+    orientation=1
+    maze = mazeObject.getMaze()
+    for move in genome:
+        dx_d, dy_d = directions[move]
+        next_X = x+dx_d
+        next_Y = y+dy_d
+        
+        mazeObject.updateOrientation(move)
+        
+        if 0 <= next_Y < len(maze) and 0 <= next_X < len(maze[0]):
+            if maze[next_Y][next_X] == 0:
+                # Le mouvement est possible
+                x = next_X
+                y = next_Y
+                mazeObject.updateX(x)
+                mazeObject.updateY(y)
+                
+        mazeObject.clear()
+        mazeObject.printMaze()
+        time.sleep(0.3)
+        if x==3 and y==0:
+            break        
 
 keepExecute = True
 while keepExecute:
@@ -29,6 +64,7 @@ Enter what you want to do : \n 1 : execute the maze resolution by right hand rul
         gaResults, best_genome = executeGA()
         print(f"The best individual solved the maze the following way : {best_genome}")
         print()
+        printGaMaze(genome=best_genome)       
         
         
 
@@ -37,15 +73,15 @@ try:
     plt.figure(figsize=(12,5))
     
     plt.subplot(1,2,1)
-    plt.plot(gaResults, label="Genetic algorithm", color="plum")
+    plt.plot(gaResults, label="Genetic algorithm", color="plum", linewidth=2)
     plt.title("Learning rate Genetic Algorithm")
     plt.xlabel("Generation")
-    plt.ylabel("Score (distance to exit + steps)")
+    plt.ylabel("Score")
     plt.legend()
     plt.grid(True,alpha=0.3)
     
     plt.subplot(1,2,2)
-    final_steps_ga = gaResults[-1]*10
+    final_steps_ga = gaResults[-1]*100
     methods = ["Right Hand", "Genetic Algorithm"]
     steps = [rhSteps, final_steps_ga]
     plt.bar(methods, steps, color=["salmon","chartreuse"])
